d8b6097fb6531dbb481da730c6bfc0f1263e0189
James Rowlands <james.rowlands@stagecoach-technology.com>
2019-05-21 19:52:40 +0100

    Undoing a suggest lambda change by intellij. Its not as clear whats happening. Adding readme

--- src/main/java/com/rowlands/primenumber/primenumbergenerator/feature/generate/v1/endpoint/PrimeNumberGeneratorController.java
+++ src/main/java/com/rowlands/primenumber/primenumbergenerator/feature/generate/v1/endpoint/PrimeNumberGeneratorController.java
@@ -4,14 +4,12 @@ import com.rowlands.primenumber.primenumbergenerator.feature.generate.v1.api.Pri
 import com.rowlands.primenumber.primenumbergenerator.feature.generate.v1.service.PrimeNumberGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.validation.constraints.NotBlank;
 import java.util.Optional;
 
 
@@ -26,11 +24,12 @@ public class PrimeNumberGeneratorController {
     public PrimeNumberResponse handleUpdate(@PathVariable int number,
                                             @RequestParam("algorithm_name") Optional<String> algorithm_name) {
 
-        return algorithm_name.map(
-                s -> new PrimeNumberResponse(primeNumberGeneratorService.getPrimesFor(number, s))).orElseGet(
-                    () -> new PrimeNumberResponse(primeNumberGeneratorService.getPrimesFor(number, "default")
-                )
-        );
 
+        if(algorithm_name.isPresent()) {
+            return new PrimeNumberResponse(primeNumberGeneratorService.getPrimesFor(number, algorithm_name.get()));
+        }
+        else {
+            return new PrimeNumberResponse(primeNumberGeneratorService.getPrimesFor(number, "default"));
+        }
     }
 }
