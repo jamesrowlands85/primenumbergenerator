520096f317649b4f64459a090822d089e286bc71
James Rowlands <james.rowlands@stagecoach-technology.com>
2019-06-02 16:57:14 +0100

    Removing unused exception.

--- src/test/java/com/rowlands/primenumber/primenumbergenerator/feature/generate/v1/endpoint/PrimeNumberGeneratorControllerTest.java
+++ src/test/java/com/rowlands/primenumber/primenumbergenerator/feature/generate/v1/endpoint/PrimeNumberGeneratorControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import javax.ws.rs.BadRequestException;
 import java.util.Optional;
 
 import static org.hamcrest.core.Is.is;
